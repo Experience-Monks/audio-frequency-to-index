d4832805cf5b4c355d9bb4ecd555e4e3b8f3aecf
mattdesl <dave.des@gmail.com>
2015-11-25 12:22:31 -0500

    rework and improve

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var clamp = require('clamp')
 
 module.exports = frequencyToIndex
-function frequencyToIndex (freq, sampleRate, fftSize) {
-  return clamp(Math.floor(freq / (sampleRate / fftSize)), 0, fftSize / 2)
+function frequencyToIndex (frequency, sampleRate, fftSize) {
+  var nyquist = sampleRate / 2
+  var length = fftSize / 2
+  var index = Math.round(frequency / nyquist * length)
+  return clamp(index, 0, length)
 }
--- test.js
+++ test.js
@@ -2,7 +2,8 @@ var freq2index = require('./')
 var test = require('tape')
 
 test('convert a Hz frequency to an index for WebAudio', function (t) {
-  t.equal(freq2index(800, 44100, 1024), 18)
+  t.equal(freq2index(800, 44100, 1024), 19)
+  t.equal(freq2index(400, 44100, 2048), 19)
   t.equal(freq2index(0, 44100, 1024), 0)
   t.equal(freq2index(4100, 44100, 1024), 95)
   t.equal(freq2index(44100, 44100, 1024), 512)
